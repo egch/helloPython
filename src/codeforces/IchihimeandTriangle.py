63882d17175a80cc228470e143bf20de9aefd198
Enrico Giurin <enricogiurin@gmail.com>
2020-04-17 06:32:26 +0200

    c1337 problem A

---
+++ src/codeforces/IchihimeandTriangle.py
@@ -0,0 +1,7 @@
+# https://codeforces.com/contest/1337/problem/A
+count = int(input())
+while count > 0:
+    a, b, c, d = map(int, input().split())
+    msg = "%s %s %d" % (b, c, c)
+    print(msg)
+    count = count - 1
